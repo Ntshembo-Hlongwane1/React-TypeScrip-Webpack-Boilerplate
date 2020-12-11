216635f0807e805960c5bafe0807b92a7887bc88
Ntshembo <juniorWebProjects@gmail.com>
2020-12-11 20:36:47 +0200

    Fix: Webpack to build and run TypeScript / React

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,14 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
     ],
   },
 
